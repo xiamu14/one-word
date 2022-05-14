8cb31b1d898ecb808b1638d2e7615392298eb3de
ben <x_z_gfree@163.com>
2022-05-15 01:30:33 +0800

    修改 调整 blockquate ，step 块样式

--- components/step/index.tsx
+++ components/step/index.tsx
@@ -1,10 +1,19 @@
-function Step({ number, title }: { number: string; title: string }) {
+import { PropsWithChildren } from "react";
+
+function Step({
+  number,
+  title,
+  children,
+}: PropsWithChildren<{ number: string; title: string }>) {
   return (
-    <div className="step flex items-center py-1">
-      <div className="flex items-center justify-center border border-gray-200 pt-1 font-bold dark:border-gray-800 rounded-full h-7 w-7 text-blue-500">
-        {number}
+    <div>
+      <div className="step flex items-center py-1">
+        <div className="flex items-center justify-center border border-gray-300 font-bold dark:border-gray-800 rounded-full h-7 w-7 text-blue-500">
+          {number}
+        </div>
+        <h4 className="ml-2 tracking-tight">{title}</h4>
       </div>
-      <h4 className="ml-2 tracking-tight">{title}</h4>
+      <div className="mx-[36px]">{children}</div>
     </div>
   );
 }
